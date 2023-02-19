a04a45485bc7724a885845e9cc29ee1e53a33aef
msnhdyt <msnurhidayat12@gmail.com>
2023-02-19 10:22:57 +0700

    add storage feature

--- gen-12-tugas-react-redux-toolkit/src/utils/api-request.js
+++ gen-12-tugas-react-redux-toolkit/src/utils/api-request.js
@@ -1,13 +1,30 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3000/notes/'
+const NOTES_KEY = 'NOTES'
 
 const getAllNotes = async () => {
   try {
     const response = await axios.get(BASE_URL)
     return response.data
   } catch (error) {
-    alert(error.message)
+    // alert(error.message)
+    if (error.code === 'ERR_NETWORK') {
+      let notes = JSON.parse(localStorage.getItem(NOTES_KEY))
+      if (!notes) {
+        notes = [
+          {
+            id: new Date().getTime(),
+            title: 'your title note',
+            createdAt: new Date().toISOString(),
+            body: 'your note',
+            archived: false
+          }
+        ]
+        localStorage.setItem(NOTES_KEY, JSON.stringify(notes))
+      }
+      return notes
+    }
   }
 }
 
@@ -16,7 +33,12 @@ const getNote = async (id) => {
     const note = await axios.get(BASE_URL + id)
     return note.data
   } catch (error) {
-    alert(error.message)
+    // alert(error.message)
+    if (error.code === 'ERR_NETWORK') {
+      let notes = JSON.parse(localStorage.getItem(NOTES_KEY))
+      const note = notes.filter((note) => note.id == id)[0]
+      return note
+    }
   }
 }
 
@@ -24,7 +46,11 @@ const addNote = async (note) => {
   try {
     await axios.post(BASE_URL, note)
   } catch (error) {
-    alert(error.message)
+    // alert(error.message)
+    if (error.code === 'ERR_NETWORK') {
+      let notes = JSON.parse(localStorage.getItem(NOTES_KEY)) || []
+      localStorage.setItem(NOTES_KEY, JSON.stringify([...notes, { ...note, id: new Date().getTime() }]))
+    }
   }
 }
 
@@ -32,15 +58,30 @@ const deleteNote = async (id) => {
   try {
     await axios.delete(BASE_URL + id)
   } catch (error) {
-    alert(error.message)
+    // alert(error.message)
+    if (error.code === 'ERR_NETWORK') {
+      let notes = JSON.parse(localStorage.getItem(NOTES_KEY))
+      const filteredNotes = notes.filter((note) => note.id != id)
+      localStorage.setItem(NOTES_KEY, JSON.stringify([...filteredNotes]))
+    }
   }
 }
 
-const updateNote = async (id, note) => {
+const updateNote = async (id, newNote) => {
   try {
-    await axios.put(BASE_URL + id, note)
+    await axios.put(BASE_URL + id, newNote)
   } catch (error) {
-    alert(error.message)
+    // alert(error.message)
+    if (error.code === 'ERR_NETWORK') {
+      let notes = JSON.parse(localStorage.getItem(NOTES_KEY))
+      const filteredNotes = notes.map((note) => {
+        if (note.id == id) {
+          return { ...newNote, id: id }
+        }
+        return note
+      })
+      localStorage.setItem(NOTES_KEY, JSON.stringify([...filteredNotes]))
+    }
   }
 }
 
@@ -49,16 +90,9 @@ const getAuthUser = async (credential) => {
     const user = await axios.post('https://dummyjson.com/auth/login', credential)
     return user.data
   } catch (error) {
-    alert("Incorrect username or password")
+    alert('Incorrect username or password')
     return {}
   }
 }
 
-export {
-  getAllNotes,
-  getNote,
-  addNote,
-  deleteNote,
-  updateNote,
-  getAuthUser
-}
+export { getAllNotes, getNote, addNote, deleteNote, updateNote, getAuthUser }
